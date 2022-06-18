89efb26c440f03145cf5b767822baab0190d700f
Kiara <kiarabroekhuizen1@hotmail.com>
2022-06-18 14:09:20 +0200

    progressbar Form works

--- public/js/script.js
+++ public/js/script.js
@@ -1,50 +1,65 @@
 console.log('are we connected?');
 
-const form = document.querySelector('form');
+const form = document.querySelector('#form form');
 const nextButtons = document.querySelectorAll('.btn-next');
-const nextBtn = document.querySelector('.btn-next')
-const submitBtn = document.querySelector('#form-submit')
-const fieldsetTwo = document.querySelector(`fieldset:nth-of-type(2)`)
-const fieldsetThree = document.querySelector(`fieldset:nth-of-type(3)`)
+const nextBtn = document.querySelector('.btn-next');
+const submitBtn = document.querySelector('#form-submit');
+const fieldsetTwo = document.querySelector(`fieldset:nth-of-type(2)`);
+const fieldsetThree = document.querySelector(`fieldset:nth-of-type(3)`);
+const progress = document.querySelector('#form .progressBar');
 let i = 1
 
+// FORM
 // when JS is enabled
-addClass(submitBtn, 'hide');
-addClass(fieldsetTwo, 'hide');
-addClass(fieldsetThree, 'hide');
-deleteClass(nextBtn, 'hide')
+if (form) {
+    addClass(submitBtn, 'hide');
+    addClass(fieldsetTwo, 'hide');
+    addClass(fieldsetThree, 'hide');
+    deleteClass(nextBtn, 'hide');
+
+    nextButtons.forEach(e => e.addEventListener('click', () => {
+        updateFormSteps()
+        updateProgressBar()
+
+    }));
+
+    // adds class to element
+    function addClass(element, className) {
+        element.classList.add(className);
+    };
+
+    //removes class from element
+    function deleteClass(element, className) {
+        element.classList.remove(className);
+    }
+
+    function updateFormSteps() {
+        document.querySelector(`fieldset:nth-of-type(${i})`).classList.remove('show');
+        i++;
+        console.log('i =', i);
+        document.querySelector(`fieldset:nth-of-type(${i})`).classList.add('show');
+        showSubmitBtn();
+
+    }
+
+    function showSubmitBtn() {
+        if (i >= 3) {
+            submitBtn.classList.remove('hide');
+            nextBtn.classList.add('hide');
+        }
+    }
 
 
-nextButtons.forEach(e => e.addEventListener('click', () => {
-    nextQuestions()
-    loadProgressBar()
+    function updateProgressBar() {
+        const width = i / 3
+        progress.style.width = width * 100 + '%';
 
-}));
-
-// adds class to element
-function addClass(element, className) {
-    element.classList.add(className);
-};
-
-//removes class from element
-function deleteClass(element, className){
-    element.classList.remove(className);
-}
-
-function nextQuestions() {
-    document.querySelector(`fieldset:nth-of-type(${i})`).classList.remove('show');
-    i++;
-    console.log('i =', i);
-    document.querySelector(`fieldset:nth-of-type(${i})`).classList.add('show');
-    showSubmitBtn();
-
-}
-
-function showSubmitBtn() {
-    if (i >= 3) {
-        submitBtn.classList.remove('hide');
-        nextBtn.classList.add('hide');
     }
 }
+
+// END FORM
+
+
+
 
 
